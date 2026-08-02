97740d3eb34f75de37f35265cf19636c3a5bd475
muhammad-danish-webdev <myounas.dev@gmail.com>
2026-08-02 11:43:53 +0500

    Header Completed

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -1,13 +1,24 @@
 // const hamburgerBtn = document.querySelector(".hamburger-btn");
+// const navMenu = document.querySelector(".header-navbar-container");
 
 // hamburgerBtn.addEventListener("click", () => {
 //   hamburgerBtn.classList.toggle("active");
+//   navMenu.classList.toggle("active");
 // });
-
 const hamburgerBtn = document.querySelector(".hamburger-btn");
 const navMenu = document.querySelector(".header-navbar-container");
+const overlay = document.querySelector(".overlay");
 
 hamburgerBtn.addEventListener("click", () => {
   hamburgerBtn.classList.toggle("active");
   navMenu.classList.toggle("active");
+  overlay.classList.toggle("active");
+});
+
+// Close menu when clicking on the overlay
+// (Overlay par click karne par menu ko band karein)
+overlay.addEventListener("click", () => {
+  hamburgerBtn.classList.remove("active");
+  navMenu.classList.remove("active");
+  overlay.classList.remove("active");
 });
