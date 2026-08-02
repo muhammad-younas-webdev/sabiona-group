e1f379917986a3e0ea25c5768fe19bdbdf9dfa10
muhammad-danish-webdev <myounas.dev@gmail.com>
2026-08-02 20:14:05 +0500

    Hero Section Responsive Completed

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -1,10 +1,3 @@
-// const hamburgerBtn = document.querySelector(".hamburger-btn");
-// const navMenu = document.querySelector(".header-navbar-container");
-
-// hamburgerBtn.addEventListener("click", () => {
-//   hamburgerBtn.classList.toggle("active");
-//   navMenu.classList.toggle("active");
-// });
 const hamburgerBtn = document.querySelector(".hamburger-btn");
 const navMenu = document.querySelector(".header-navbar-container");
 const overlay = document.querySelector(".overlay");
