e6d01785bf22a51c7491794bf6072062d3462847
muhammad-danish-webdev <myounas.dev@gmail.com>
2026-08-05 00:31:37 +0500

    Revert "Updated"
    This reverts commit 75c4eb4abbd42c8c4298d127ec321c14cb43b38e.

--- assets/JS/swiper.js
+++ assets/JS/swiper.js
@@ -1,20 +1,73 @@
 const swiper = new Swiper(".mySwiper", {
-  // ...
+  slidesPerView: 3,
+  spaceBetween: 24,
+  loop: true,
+  speed: 800,
+
   breakpoints: {
     0: {
       slidesPerView: 1,
       spaceBetween: 16,
     },
+    576: {
+      slidesPerView: 1.5,
+      spaceBetween: 20,
+    },
     768: {
       slidesPerView: 2,
-      spaceBetween: 16, // gap increase
+      spaceBetween: 24,
     },
     992: {
       slidesPerView: 3,
-      spaceBetween: 16, // gap increase
+      spaceBetween: 24,
     },
   },
 });
+//
+// Bullet Clickable JS
+// const bullets = document.querySelectorAll(".custom-pagination-bullet");
+
+// // By default first bullet active
+// bullets[0].classList.add("swiper-pagination-bullet-active");
+
+// // Click event
+// bullets.forEach((bullet) => {
+//   bullet.addEventListener("click", () => {
+//     // Sab se active class remove
+//     bullets.forEach((item) => {
+//       item.classList.remove("swiper-pagination-bullet-active");
+//     });
+
+//     // Jis par click hua us par add
+//     bullet.classList.add("swiper-pagination-bullet-active");
+//   });
+// });
+
+// const bullets = document.querySelectorAll(".custom-pagination-bullet");
+// const cardImages = document.querySelectorAll(".swiper-card-image");
+
+// // By default
+// bullets[0].classList.add("swiper-pagination-bullet-active");
+// cardImages[0].classList.add("swiper-card-image-active");
+
+// bullets.forEach((bullet, index) => {
+//   bullet.addEventListener("click", () => {
+//     // Bullet active remove
+//     bullets.forEach((item) => {
+//       item.classList.remove("swiper-pagination-bullet-active");
+//     });
+
+//     // Image active remove
+//     cardImages.forEach((item) => {
+//       item.classList.remove("swiper-card-image-active");
+//     });
+
+//     // Active add
+//     bullet.classList.add("swiper-pagination-bullet-active");
+//     cardImages[index].classList.add("swiper-card-image-active");
+//   });
+// });
+
 const bullets = document.querySelectorAll(".custom-pagination-bullet");
 const cardImages = document.querySelectorAll(".swiper-card-image");
 const cards = document.querySelectorAll(".portfolio-card"); // Card elements
